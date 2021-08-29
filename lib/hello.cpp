5701d5b37ba746a44d571e3c9262cd56647ce948
katanik <32272478+katanik@users.noreply.github.com>
2021-08-29 23:30:31 +0700

    Added build number printing (#15)

--- lib/hello.cpp
+++ lib/hello.cpp
@@ -1,6 +1,18 @@
 #include "hello.h"
+#include "build_number.h"
 
 std::ostream& hello::greetings(std::ostream &out)
 {
-    return out<<"Hello, world!";
+    return out<<"Hello, world!"<<std::endl;
 }
+
+std::ostream& hello::printBuildNumber(std::ostream &out)
+{
+	return out << "build " << getBuildNumber() << std::endl;
+}
+
+int hello::buildNumber()
+{
+    return getBuildNumber();
+}
+
--- tests/test_hello.cpp
+++ tests/test_hello.cpp
@@ -1,10 +1,19 @@
 #include <gtest/gtest.h>
-
 #include <hello.h>
 
 TEST(Hello, simple)
 {
     std::stringstream ss;
     hello::greetings(ss);
-    ASSERT_EQ("Hello, world!", ss.str());
+    ASSERT_EQ("Hello, world!\n", ss.str());
+}
+
+TEST(BuildNumber, simple)
+{
+    std::stringstream ss;
+    hello::printBuildNumber(ss);
+
+    std::stringstream result;
+    result<<"build "<<hello::buildNumber()<<std::endl;
+    ASSERT_EQ(result.str(), ss.str());
 }
